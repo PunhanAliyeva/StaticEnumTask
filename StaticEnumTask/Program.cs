5736ada9e7ac3ce77fc723af17a0ed947c64c0ce
PunhanAliyeva <eliyevapunhan567@gmail.com>
2024-04-07 21:16:03 +0400

    Add project files.

---
+++ StaticEnumTask/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace StaticEnumTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[0];
+            Array.Resize(ref arr, 4);
+            arr[0] = 1;
+            arr[1] = 2;
+            arr[2] = 3;
+            arr[3] = 4;
+            int sum = 0;
+            foreach (var item in arr)
+            {
+                sum += item;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
